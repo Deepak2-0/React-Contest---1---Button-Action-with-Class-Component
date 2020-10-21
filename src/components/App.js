6197d5f161dfc2ea1b0f1c117394a8bc5121e397
Deepak Choudhary <choudharydeepak938@gmail.com>
2020-10-21 20:27:26 +0530

    Initial Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,33 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+  state = {
+    id: "click",
+    para: ""
+  };
+
+  handleClick = () => {
+    this.setState({
+      id: "para",
+      para:
+        "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+    });
+  };
+
+  render() {
+    return (
+      <div id="main">
+        <button onClick={this.handleClick} id={this.state.id}>
+          {this.state.para}
+        </button>
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
